0155f4d3004ac2fda9522d41c4038398c7983602
Milad Joodi <winbeta.com@gmail.com>
2024-02-27 01:40:42 +0330

    0

--- app/create-form.tsx
+++ app/create-form.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import { useFormState, useFormStatus } from 'react-dom'
 import { createProduct } from '@/lib/actions'
@@ -6,72 +6,86 @@ import { useEffect, useRef } from 'react'
 import toast from 'react-hot-toast'
 
 export default function CreateForm() {
-    const [state, formAction] = useFormState(createProduct, {
-        message: '',
-    })
-    const { pending } = useFormStatus()
-    const ref = useRef<HTMLFormElement>(null)
-    useEffect(() => {
-        if (state.message.indexOf('Created product') === 0) {
-            document.getElementById('my_modal_3')!.close()
-            ref.current?.reset()
-            toast(state.message)
-        } else if (state.message) {
-            toast(state.message)
-        }
-    }, [state.message])
+  const [state, formAction] = useFormState(createProduct, {
+    message: '',
+  })
+  const { pending } = useFormStatus()
+  const ref = useRef<HTMLFormElement>(null)
+  useEffect(() => {
+    if (state.message.indexOf('Created product') === 0) {
+      ;(document.getElementById('my_modal_3') as any)!.close()
+      ref.current?.reset()
+      toast(state.message)
+    } else if (state.message) {
+      toast(state.message)
+    }
+  }, [state.message])
 
-    return (
-        <div>
-            <button className='btn btn-primary' onClick={() => document.getElementById('my_modal_3')!.showModal()}
+  return (
+    <div>
+      <button
+        className="btn btn-primary"
+        onClick={() =>
+          (document.getElementById('my_modal_3')! as any).showModal()
+        }
+      >
+        Create Product
+      </button>
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <h2 className="tex-2xl font-bold pm-4">Create Product</h2>
+          <form ref={ref} action={formAction}>
+            <div className="form-control w-full max-w-xs py-4">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="input input-bordered w-full max-w-xs"
+                required
+              />
+            </div>
+            <div className="form-control w-full max-w-xs py-4">
+              <label htmlFor="image">Image</label>
+              <input
+                type="text"
+                id="image"
+                name="image"
+                className="input input-bordered w-full max-w-xs"
+                required
+                defaultValue="/images/shirt1.jpg"
+              />
+            </div>
+            <div className="form-control w-full max-w-xs py-4">
+              <label htmlFor="price">Price</label>
+              <input
+                type="number"
+                id="price"
+                name="price"
+                className="input input-bordered w-full max-w-xs"
+                required
+                defaultValue="1"
+              />
+            </div>
+            <button
+              className="btn btn-primary mr-3"
+              type="submit"
+              disabled={pending}
             >
-                Create Product
+              Create
             </button>
-            <dialog id='my_modal_3' className='modal'>
-                <div className='modal-box'>
-                    <h2 className='text-2xl font-bold pm-4'>Create Product</h2>
-                    <form ref={ref} action={formAction}>
-                        {/* INPUT NAME */}
-                        <div className='form-control w-full max-w-xs py-4'>
-                            <label htmlFor="name">Name</label>
-                            <input
-                                type="text"
-                                id='name'
-                                name='name'
-                                className='input input-bordered w-full max-w-xs'
-                                required
-                            />
-                        </div>
-                        {/* INPUT IMAGE */}
-                        <div className='form-control w-full max-w-xs py-4'>
-                            <label htmlFor="image">Image</label>
-                            <input
-                                type="text"
-                                id='image'
-                                name='image'
-                                className='input input-bordered w-full max-w-xs'
-                                required
-                                defaultValue="/images/shirt1.jpg"
-                            />
-                        </div>
-                        {/* INPUT PRICE */}
-                        <div className='form-control w-full max-w-xs py-4'>
-                            <label htmlFor="price">Price</label>
-                            <input
-                                type="number"
-                                id='price'
-                                name='price'
-                                className='input input-bordered w-full max-w-xs'
-                                required
-                                defaultValue="1"
-                            />
-                        </div>
-                        {/* BUTTON create and back */}
-                        <button className='btn btn-primary mr-3' type='submit' disabled={pending}>Create</button>
-                        <button className='btn btn-ghost' type='button' onClick={()=> document.getElementById('my_modal_3')?.close()}>Back</button>
-                    </form>
-                </div>
-            </dialog>
+            <button
+              type="button"
+              className="btn btn-ghost"
+              onClick={() =>
+                (document.getElementById('my_modal_3') as any).close()
+              }
+            >
+              Back
+            </button>
+          </form>
         </div>
-    )
+      </dialog>
+    </div>
+  )
 }
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,59 +1,55 @@
-"use server"
+'use server'
 import { revalidatePath } from 'next/cache'
 import ProductModel from './product-model'
 import dbConnect from './db-connect'
 import { z } from 'zod'
-import { parse } from 'path'
 
-// CREATE_ACTION
 export async function createProduct(prevState: any, formData: FormData) {
-    const schema = z.object({
-        name: z.string().min(3),
-        image: z.string().min(1),
-        price: z.number().min(1),
-        rating: z.number().min(1).max(5)
-    })
-    const parse = schema.safeParse({
-        name: formData.get("name"),
-        image: formData.get("image"),
-        price: Number(formData.get("price")),
-        rating: Math.ceil(Math.random() * 5),
-    })
-    if (!parse.success){
-        console.log(parse.error)
-        return {message: 'Form data is not valid'}
-    }
-    const data = parse.data
-    try{
-        await dbConnect()
-        const product = new ProductModel(data)
-        await product.save()
-        revalidatePath('/')
-        return {message: `Created product ${data.name}`}
-    }catch(e){
-        return {message: 'Failed to create product'}
-    }
-
+  const schema = z.object({
+    name: z.string().min(3),
+    image: z.string().min(1),
+    price: z.number().min(1),
+    rating: z.number().min(1).max(5),
+  })
+  const parse = schema.safeParse({
+    name: formData.get('name'),
+    image: formData.get('image'),
+    price: Number(formData.get('price')),
+    rating: Math.ceil(Math.random() * 5),
+  })
+  if (!parse.success) {
+    console.log(parse.error)
+    return { message: 'Form data is not valid' }
+  }
+  const data = parse.data
+  try {
+    await dbConnect()
+    const product = new ProductModel(data)
+    await product.save()
+    revalidatePath('/')
+    return { message: `Created product ${data.name}` }
+  } catch (e) {
+    return { message: 'Failed to create product' }
+  }
 }
 
-// DELETE_ACTION
 export async function deleteProduct(formData: FormData) {
-    const schema = z.object({
-        _id: z.string().min(1),
-        name: z.string().min(1),
-    })
-    const data = schema.parse({
-        _id: formData.get("_id"),
-        name: formData.get("name"),
-    })
-    try{
-        await dbConnect()
-        await ProductModel.findByIdAndDelete({_id: data._id})
-        revalidatePath('/')
-        console.log({message: `Deleted product ${data.name}`})
-        return {message: `Deleted product ${data.name}`}
-    }catch(e){
-        return {message: 'Failed to delete product'}
-    }
+  const schema = z.object({
+    _id: z.string().min(1),
+    name: z.string().min(1),
+  })
+  const data = schema.parse({
+    _id: formData.get('_id'),
+    name: formData.get('name'),
+  })
 
+  try {
+    await dbConnect()
+    await ProductModel.findOneAndDelete({ _id: data._id })
+    revalidatePath('/')
+    console.log({ message: `Deleted product ${data.name}` })
+    return { message: `Deleted product ${data.name}` }
+  } catch (e) {
+    return { message: 'Failed to delete product' }
+  }
 }
